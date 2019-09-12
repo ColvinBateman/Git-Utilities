bdd57a4e2915e63b1126ee0c60ca79aa9f1ac387
ColvinBateman <46699025+ColvinBateman@users.noreply.github.com>
2019-09-12 11:47:12 -0600

    More things

---
+++ CrashCourse.cs
@@ -0,0 +1,18 @@
+﻿//Putting a const in front of something makes it only allowed to have the value assigned to it upfront
+//for instance, if you have a const bool of true, and try to make of false, you can't.
+
+//If I want a string, and am not sure about the amount of things I want in the string, I can simply say something like
+//string[] things = new string[5]
+
+//If I want a loop to go through an array, I can do something like the following example
+//for (int i=0; i < writers.Length; i++)//for the new int of i, as long as i is less than the writers array, and each loop makes i go up one,
+//{
+//  string writer = writers[i];//assigns the current writer in the string to the current i value
+//  Console.WriteLine(writer);//tells you what the writer is in the earlier string
+//}
+//Alternatively
+//foreach (string writer in writers)//for every defined writer in the writers string
+//{
+//  Console.WriteLine(writer);//says the writer from the earlier string
+//}
+
---
+++ InstantiateObject.cs
@@ -0,0 +1,102 @@
+﻿//Definitions
+
+//original - an object that exists already, intended to be copied
+//position - position for new object
+//rotation - rotation ''
+//parent - parent that will be assigned to the object
+//instantiateInWorldSpace - Pass true when assigning a parent Object to maintain the world position of the Object, instead of setting its position relative to the new parent. Pass false to set the Object's position relative to its new parent.
+
+/*
+// Instantiates 10 copies of Prefab each 2 units apart from each other
+
+using UnityEngine;
+using System.Collections;
+
+public class ExampleClass : MonoBehaviour
+{
+    public Transform prefab;
+    void Start()
+    {
+        for (int i = 0; i < 10; i++)//as long as the newly created i is less than ten, and i increases each loop
+        {
+            Instantiate(prefab, new Vector3(i * 2.0F, 0, 0), Quaternion.identity);//instantiate a new prefab two units apart from the last one
+        }
+    }
+}
+*/
+/*
+using UnityEngine;
+
+public class Example : MonoBehaviour
+{
+    // Instantiate a rigidbody then set the velocity
+
+    Rigidbody projectile;//creates a rigidbody named projectile
+
+    void Update()
+    {
+        // Ctrl was pressed, launch a projectile
+        if (Input.GetButtonDown("Fire1"))
+        {
+            // Instantiate the projectile at the position and rotation of this transform
+            Rigidbody clone;
+            clone = Instantiate(projectile, transform.position, transform.rotation);
+
+            // Give the cloned object an initial velocity along the current
+            // object's Z axis
+            clone.velocity = transform.TransformDirection(Vector3.forward * 10);
+        }
+    }
+}
+*/
+
+/*
+using UnityEngine;
+using System.Collections;
+
+public class Missile : MonoBehaviour
+{
+    public int timeoutDestructor;
+
+    // ...other code...
+}
+
+
+public class ExampleClass : MonoBehaviour
+{
+    // Instantiate a Prefab with an attached Missile script
+    public Missile projectile;
+
+    void Update()
+    {
+        // Ctrl was pressed, launch a projectile
+        if (Input.GetButtonDown("Fire1"))
+        {
+            // Instantiate the projectile at the position and rotation of this transform
+            Missile clone = (Missile)Instantiate(projectile, transform.position, transform.rotation);
+
+            // Set the missiles timeout destructor to 5
+            clone.timeoutDestructor = 5;
+        }
+    }
+}
+*/
+
+/*
+using UnityEngine;
+
+public class Missile : MonoBehaviour
+{
+    // ...other code...
+}
+
+public class InstantiateGenericsExample : MonoBehaviour
+{
+    public Missile missile;//creates a public class of Missile, and makes missile the field
+
+    void Start()
+    {
+        Missile missileCopy = Instantiate<Missile>(missile);//instantiates a copy of the missile
+    }
+}
+*/
